420fb8bafdc5afe366804b276bd4597b49e5a115
Marco Teixeira <marco.teixeira4@fatec.sp.gov.br>
2020-01-31 10:59:31 -0300

    controle de Mensagens de Erro

--- js/scripts.js
+++ js/scripts.js
@@ -15,28 +15,26 @@ $(function () {
 
 
 
-    //objeto criado para não ter que passar sempre cidade, estado e pais 
     var weatherObject = {
         cidade: "",
         estado: "",
-        pais: "",
+        pais : "",
         temperatura: "",
         texto_clima: "",
         icone_clima: ""
     };
 
-    function preencherClimaAgora(cidade, estado, pais, temperatura, texto_clima, icone_clima) {
+    function preencherClimaAgora(cidade,estado,pais,temperatura, texto_clima, icone_clima) {
 
         var texto_local = cidade + ", " + estado + ". " + pais;
         $("#texto_local").text(texto_local);
         $("#texto_clima").text(texto_clima);
-        $("#texto_temperatura").html(String(temperatura) + "&deg;");
-        $("#icone_clima").css("background-image", "url('" + weatherObject.icone_clima + "')");
+        $("#texto_temperatura").html( String(temperatura) + "&deg;" );
+        $("#icone_clima").css("background-image", "url('" + weatherObject.icone_clima + "')" );
 
     }
 
     function gerarGrafico(horas, temperaturas) {
-
         Highcharts.chart('hourly_chart', {
             chart: {
                 type: 'line'
@@ -44,13 +42,13 @@ $(function () {
             title: {
                 text: 'Temperatura Hora a Hora'
             },
-
+            
             xAxis: {
-                categories:horas
+                categories: horas
             },
             yAxis: {
                 title: {
-                    text: 'Temperature (°C)'
+                    text: 'Temperatura (°C)'
                 }
             },
             plotOptions: {
@@ -64,16 +62,14 @@ $(function () {
             series: [{
                 showInLegend: false,
                 data: temperaturas
-
             }]
         });
     }
 
-    gerarGrafico();
+    
 
-    function pegarPrevisaoHoraAHora(localCode) {
+    function pegarPrevisãoHoraAHora(localCode) {
         
-
         $.ajax({
             url : "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/" + localCode + "?apikey=" + accuweatherAPIKey +"&language=pt-br&metric=true",
             type: "GET",
@@ -84,129 +80,126 @@ $(function () {
                 var horarios = [];
                 var temperaturas = [];
 
-                for( var i = 0; i < data.length; i++){
-                    var hora = new Date( data[i].DateTime ).getHours();
+                for (var a = 0; a < data.length; a++) {
 
-                    horarios.push( String(hora) + "h" );//ex: 21h
-                    temperaturas.push( data[i].Temperature.Value );
+                    var hora = new Date( data[a].DateTime ).getHours();
+
+                    horarios.push( String(hora) + "h" );
+                    temperaturas.push( data[a].Temperature.Value )
                     
                     gerarGrafico(horarios, temperaturas);
                     $('.refresh-loader').fadeOut();
+
                 }
+                
             },
-            error: function () {
+            error: function(){
                 console.log("Erro");
-                gerarErro("Erro ao obter a hora");
-                $('.refresh-loader').fadeOut();
-
-            }
+                gerarErro("Erro ao obter a previsão hora a hora");
+            
+            }  
         });
-
+    
     }
-
 
     function preencherPrevisao5Dias(previsoes) {
 
         $("#info_5dias").html("");
         var diasSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
 
-        for (var i = 0; i < previsoes.length; i++) {
+        for (var a = 0; a < previsoes.length; a++) {
 
-            var dataHoje = new Date(previsoes[i].Date);
-            var dia_semana = diasSemana[dataHoje.getDay()];
+            var dataHoje = new Date(previsoes[a].Date);
+            var dia_semana = diasSemana[ dataHoje.getDay() ];
 
-            var iconNumber = previsoes[i].Day.Icon <= 9 ? "0" + String(previsoes[i].Day.Icon) : String(previsoes[i].Day.Icon);
+            var iconNumber = previsoes[a].Day.Icon <= 9 ? "0" + String(previsoes[a].Day.Icon) : String(previsoes[a].Day.Icon);
 
             iconeClima = "https://developer.accuweather.com/sites/default/files/" + iconNumber + "-s.png";
-            maxima = String(previsoes[i].Temperature.Maximum.Value);
-            minima = String(previsoes[i].Temperature.Minimum.Value);
+            maxima = String(previsoes[a].Temperature.Maximum.Value);
+            minima = String(previsoes[a].Temperature.Minimum.Value);
 
-            elementoHTMLDia = '<div class="day col">';
-            elementoHTMLDia += '<div class="day_inner">';
-            elementoHTMLDia += '<div class="dayname">';
-            elementoHTMLDia += dia_semana;
-            elementoHTMLDia += '</div>';
-            elementoHTMLDia += '<div style="background-image: url(\'' + iconeClima + '\')" class="daily_weather_icon"></div>'
-            elementoHTMLDia += '<div class="max_min_temp">';
-            elementoHTMLDia += minima + '&deg; / ' + maxima + '&deg;';
-            elementoHTMLDia += '</div>';
-            elementoHTMLDia += '</div>';
-            elementoHTMLDia += '</div>';
-
+            elementoHTMLDia =  '<div class="day col">';
+            elementoHTMLDia +=    '<div class="day_inner">';
+            elementoHTMLDia +=        '<div class="dayname">';
+            elementoHTMLDia +=              dia_semana;
+            elementoHTMLDia +=        '</div>';
+            elementoHTMLDia +=        '<div style="background-image: url(\'' + iconeClima + '\')" class="daily_weather_icon"></div>'   
+            elementoHTMLDia +=        '<div class="max_min_temp">';
+            elementoHTMLDia +=          minima + '&deg; / ' + maxima + '&deg;';    
+            elementoHTMLDia +=        '</div>';
+            elementoHTMLDia +=    '</div>';    
+            elementoHTMLDia += '</div>';       
+                
             $("#info_5dias").append(elementoHTMLDia);
             elementoHTMLDia = "";
+        
 
         }
 
     }
 
     function pegarPrevisao5Dias(localCode) {
-
+    
         $.ajax({
-            url: "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + localCode + "?apikey=" + accuweatherAPIKey + "&language=pt-br&metric=true",
+            url : "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + localCode + "?apikey=" + accuweatherAPIKey + "&language=pt-br&metric=true",
             type: "GET",
             dataType: "json",
-            success: function (data) {
-                console.log("5 day forecast: ", data);
+            success: function(data){
+                // console.log("5 day forecast: ", data);
 
-                $("#texto_max_min").html(String(data.DailyForecasts[0].Temperature.Minimum.Value) + "&deg; / " + String(data.DailyForecasts[0].Temperature.Maximum.Value) + "&deg;");
+                $("#texto_max_min").html( String(data.DailyForecasts[0].Temperature.Minimum.Value) + "&deg; / " + String(data.DailyForecasts[0].Temperature.Maximum.Value) + "&deg;");
 
                 preencherPrevisao5Dias(data.DailyForecasts);
+                
             },
-            error: function () {
+            error: function(){
                 console.log("Erro");
                 gerarErro("Erro ao obter a previsão de 5 dias");
-                $('.refresh-loader').fadeOut();
-
-            }
+            
+            }  
         });
-
+    
     }
 
-
-
-    //função para pegar o tempo atual
     function pegarTempoAtual(localCode) {
-
+        
         $.ajax({
-            url: "http://dataservice.accuweather.com/currentconditions/v1/" + localCode + "?apikey=" + accuweatherAPIKey + "&language=pt-br",
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                console.log("current conditions:", data);
+            url : "http://dataservice.accuweather.com/currentconditions/v1/" + localCode + "?apikey=" + accuweatherAPIKey + "&language=pt-br",
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                // console.log("current conditions: ", data);
 
-                //temperatura está em uma lista
                 weatherObject.temperatura = data[0].Temperature.Metric.Value;
                 weatherObject.texto_clima = data[0].WeatherText;
 
                 var iconNumber = data[0].WeatherIcon <= 9 ? "0" + String(data[0].WeatherIcon) : String(data[0].WeatherIcon);
+
                 weatherObject.icone_clima = "https://developer.accuweather.com/sites/default/files/" + iconNumber + "-s.png";
 
-                preencherClimaAgora(weatherObject.cidade, weatherObject.estado, weatherObject.pais, weatherObject.temperatura, weatherObject.texto_clima, weatherObject.icone_clima);
+                preencherClimaAgora(weatherObject.cidade,weatherObject.estado,weatherObject.pais,weatherObject.temperatura, weatherObject.texto_clima, weatherObject.icone_clima);
             },
-            error: function () {
+            error: function(){
                 console.log("Erro");
-                $('.refresh-loader').fadeOut();
-            }
-
+                gerarErro("Erro ao obter clima atual");
+            
+            }  
         });
-
 
     }
 
-
     function pegarLocalUsuario(lat, long) {
         $.ajax({
-            url: "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + accuweatherAPIKey + "&q=" + lat + "%2C" + long + "&language=pt-br",
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                console.log("geoposition :", data);
-
+            url : "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + accuweatherAPIKey + "&q=" + lat + "%2C" + long +"&language=pt-br",
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                // console.log("geposition: ", data);
+                
                 try {
-                    //parentCity não aparece sempre, então trycatch é para se tiver algum erro
                     weatherObject.cidade = data.ParentCity.LocalizedName;
-                } catch {
+                }
+                catch {
                     weatherObject.cidade = data.LocalizedName;
                 }
 
@@ -216,26 +209,26 @@ $(function () {
                 var localCode = data.Key;
                 pegarTempoAtual(localCode);
                 pegarPrevisao5Dias(localCode);
-                pegarPrevisaoHoraAHora(localCode);
+                pegarPrevisãoHoraAHora(localCode);
+
+
             },
-            error: function () {
-                console.log("Erro");
-                $('.refresh-loader').fadeOut();
-            }
-
+            error: function(){
+            console.log("Erro");
+            gerarErro("Erro no código do local");
+            
+            }  
         });
-
     }
 
     function pegarCoordenadasDaPesquisa(input) {
-       
-       input = encodeURI("input");
+        input = encodeURI(input);
         $.ajax({
-            url: "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ input +".json?access_token="+ mapboxToken,
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                //console.log("mapbox :", data);
+            url : "https://api.mapbox.com/geocoding/v5/mapbox.places/" + input + ".json?access_token=" + mapboxToken,
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                //console.log("mapbox: ", data);
                 try {
                     var long = data.features[0].geometry.coordinates[0];
                     var lat = data.features[0].geometry.coordinates[1];
@@ -243,39 +236,59 @@ $(function () {
                 } catch {
                     gerarErro("Erro na pesquisa de local");
                 }
-            },
-            error: function () {
-                console.log("Erro no Mapbox");
-                $('.refresh-loader').fadeOut();
-            }
 
+            },
+            error: function(){
+            console.log("Erro no Mapbox");
+            gerarErro("Erro na pesquisa de local");
+            
+            }  
+        });
+    }
+
+    
+
+    function pegarCoordenadasDoIP() {
+
+            var lat_padrao = -22.981361;
+            var long_padrao = -43.223176;
+
+        $.ajax({
+            url : "http://www.geoplugin.net/json.gp",
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                
+                if (data.geoplugin_latitude && data.geoplugin_longitude) {
+                    pegarLocalUsuario(data.geoplugin_latitude,data.geoplugin_longitude);
+                } else {
+                    pegarLocalUsuario(lat_padrao,long_padrao);
+                }
+
+            },
+            error: function(){
+                console.log("Erro");
+                pegarLocalUsuario(lat_padrao,long_padrao);
+            
+            }  
         });
 
     }
 
-    function pegarCoordenadasDoIP() {
-        var lat_padrao = -22.981361; // essas variaveis é para o caso se der erro
-        var long_padrao = -43.223176;
+    function gerarErro(mensagem) {
 
-        $.ajax({
-            url: "http://www.geoplugin.net/json.gp",
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
+        if(!mensagem) {
+            mensagem = "Erro na solicitação";
+        }
 
-                if (data.geoplugin_latitude && data.geoplugin_longitude) {
-                    pegarLocalUsuario(data.geoplugin_latitude, data.geoplugin_longitude);
-                } else {
-                    pegarLocalUsuario(lat_padrao, long_padrao);
-                }
+        $('.refresh-loader').hide();
+        $("#aviso-erro").text(mensagem);
+        $("#aviso-erro").slideDown();
+        window.setTimeout(function(){
+            $("#aviso-erro").slideUp();
+        },4000);
 
-            },
-            error: function () {
-                console.log("Erro");
-                pegarLocalUsuario(lat_padrao, long_padrao);
-            }
 
-        });
     }
 
     pegarCoordenadasDoIP();
@@ -303,5 +316,7 @@ $(function () {
         }
         
     });
+
+
 
 }); //fechamento da função principal "$(function()"
